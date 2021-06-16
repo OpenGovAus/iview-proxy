c993457dd769a40cd709336449ca77df5ae573f2
king-millez <millez.dev@gmail.com>
2021-06-16 16:05:43 +1000

    [search] "load more" button

--- src/static/js/search.js
+++ src/static/js/search.js
@@ -7,21 +7,29 @@ $(document).ready(function(){
 
 // This function sends a POST request to the API that handles iView searches, that we you can find shows.
 // I excluded any analytics from this request that is usually used in the search bar.
-function callSearch(text) {
-    $('.searchresponse').html('')
+function callSearch(text, page, fromLoadMore) {
+    if(!page || text.length == 0) {
+        page = 0
+    }
+    if(fromLoadMore != true) {
+        $('.searchresponse').html('')
+    }
+    $('#loadMore').remove()
+    let globalPage = page
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
         if (xhr.readyState == XMLHttpRequest.DONE) {
             let jsonReturn = JSON.parse(xhr.response).results[0].hits
-            // filter response results, dont include anything without an id or a title
-            jsonReturn = jsonReturn.filter((item) => typeof item.id !== 'undefined')
-            jsonReturn = jsonReturn.filter((item) => typeof item.title !== 'undefined')
+            // filter response results, don't include anything without an id or a title
+            jsonReturn = jsonReturn.filter((item) => typeof item.id !== 'undefined' && typeof item.title !== 'undefined')
             // Only get series/episodes
             filters = [{label: 'docType', value: 'Program'}, {label: 'docType', value: 'VideoEpisode'}]
             jsonReturn = jsonReturn.filter(candidate => candidate.docType == 'Program' || candidate.docType == 'VideoEpisode')
             for(var i = 0; i < jsonReturn.length; i++) {
                 $('.searchresponse').append(`<p><a href="/show?id=${jsonReturn[i].id}">${jsonReturn[i].title}</a></p>`)
             }
+            globalPage++
+            $('.searchresponse').append(`<button id="loadMore" onclick="callSearch(\'${text}\', ${globalPage}, true);">Load more</button>`)
         }
     }
     xhr.open("POST", 'https://y63q32nvdl-dsn.algolia.net/1/indexes/*/queries?x-algolia-api-key=bcdf11ba901b780dc3c0a3ca677fbefc&x-algolia-application-id=Y63Q32NVDL', true);
@@ -30,7 +38,8 @@ function callSearch(text) {
             {
                 "indexName": "ABC_production_iview_web",
                 "params": "query=" + text,
-                "hitsPerPage": 25
+                "hitsPerPage": 25,
+                "page": page
             }
         ]
     }));
